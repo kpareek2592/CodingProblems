fb4311cd93f12fa7fbcb51f91c5bb085cfc31f59
kaushal_pareek <kaushal.pareek@preqin.com>
2020-01-18 16:10:04 +0530

    Initial Commit. Reverse string

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using ConsoleApp1.ReverseString;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RS1.ReverseString("Apple"));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/ReverseString/RS1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1.ReverseString
+{
+    public static class RS1
+    {
+        // --- Directions
+        // Given a string, return a new string with the reversed
+        // order of characters
+        // --- Examples
+        //   reverse('apple') === 'leppa'
+        //   reverse('hello') === 'olleh'
+        //   reverse('Greetings!') === '!sgniteerG'
+
+        public static string ReverseString(string str)
+        {
+            var arr = str.ToCharArray();
+            var revArr = arr.Reverse();
+            var a1 = string.Join("", revArr);
+            return a1;
+        }
+    }
+}
